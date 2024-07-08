0d6133ef16fab8618aae4687e0e241b7ed646b4e
Ritikrai304 <ritikrai4152@gmail.com>
2024-07-08 15:38:27 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,67 @@
+from tkinter import *
+import tkinter.messagebox
+import pickle 
+import tkinter.simpledialog
+root = Tk()
+root.geometry("655x333")
+root.title("To Do List by harshita")
+def addtask():
+    task = entry_task.get()
+    if task != "":
+        list_task.insert(tkinter.END, task)
+        entry_task.delete(0,tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="Warning", message="must enter a task")
+        
+    
+def deletetask():
+    try:
+        task_index = list_task.curselection()[0]
+        list_task.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title="warning",message="must select a task")
+
+def edittask():
+    selected_index = list_task.curselection()
+    if selected_index:
+        old_task = list_task.get(selected_index)
+        new_task = tkinter.simpledialog.askstring("edit task", "edit the task:", initialvalue=old_task)
+        if new_task:
+            list_task.delete(selected_index,tkinter.END)
+            list_task.insert(selected_index, new_task)
+    else:
+        tkinter.messagebox.showwarning(title="warning", message="select a task to edit")
+
+def savetask():
+    task = list_task.get(0, list_task.size())
+    print(task)
+
+#create GUI
+frame_task = Frame(root)
+frame_task.pack()
+
+list_task=Listbox(frame_task, height=3, width=50)
+list_task.pack(side=LEFT)
+
+scrollbar_tasks = Scrollbar(frame_task) 
+scrollbar_tasks.pack(side=RIGHT, fill=X)
+
+list_task.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=list_task.yview)
+
+entry_task =Entry(root, width=50)
+entry_task.pack()
+
+#buttons for the tasks"
+button_add_task=Button(root, text="Add Task",width=48,command=addtask,bg="grey")
+button_add_task.pack()
+
+button_delete_task=Button(root, text="Delete Task",width=48,command=deletetask,bg="grey")
+button_delete_task.pack()
+
+button_edit_task=Button(root, text="Edit Task",width=48,command=edittask,bg="grey")
+button_edit_task.pack()
+
+button_save_task=Button(root, text="Save Task",width=48,command=savetask,bg="grey")
+button_save_task.pack()
+root.mainloop()
---
+++ task4.py
@@ -0,0 +1,123 @@
+from tkinter import *
+from PIL import Image,ImageTk
+from random import choice
+
+root = Tk()
+root.title("Rock Paper Scissor Game By Harshita")
+root.geometry("1300x500")
+label = Label(root, text="Rock Paper Scissor Game", font = "bold", bg="grey", fg="white")
+label.grid(row=0,column=2)
+
+# Function to resize image
+def resize_image(image_path, size):
+    image = Image.open(image_path)
+    resized_image = image.resize(size)
+    return ImageTk.PhotoImage(resized_image)
+
+# Desired size for images
+image_size = (300,300)
+
+# Resized pictures
+Rock_img_user = resize_image("rock.jpg", image_size)
+paper_img_user = resize_image("paper.jpeg", image_size)
+scissor_img_user = resize_image("scissor.jpeg", image_size)
+Rock_img_computer = resize_image("rock.jpg", image_size)
+paper_img_computer = resize_image("paper.jpeg", image_size)
+scissor_img_computer = resize_image("scissor.jpeg", image_size)
+
+# insert pictures
+user_image_label = Label(root,image=Rock_img_user)
+user_image_label.grid(row=1,column=0)
+computer_image_label = Label(root, image=Rock_img_computer)
+computer_image_label.grid(row=1,column=4)
+
+#scores
+playerscore = Label(root, text=0, font="bold",fg="black")
+playerscore.grid(row=1,column=1)
+computerscore = Label(root, text=0, font="bold",fg="black")
+computerscore.grid(row=1,column=3)
+
+# indicators 
+user_indicator = Label(root, text="USER", font="bold",fg="black")
+user_indicator.grid(row=0,column=1)
+computer_indicator = Label(root, text="COMPUTER", font="bold",fg="black")
+computer_indicator.grid(row=0,column=3)
+
+# message
+messagge = Label(root,font="bold",fg="black")
+messagge.grid(row=3,column=2)
+
+#update message
+def updateMessage(x):
+    messagge['text'] = x
+
+# update user score
+def updateUserscore():
+    score = int(playerscore["text"])
+    score  = score + 1
+    playerscore["text"] = str(score)
+
+#update computer score
+def updateComputerscore():
+    score = int(computerscore["text"])
+    score  = score + 1
+    computerscore['text'] = str(score)
+
+# checking winner
+def checkWin(player,computer):
+    if(player == computer):
+        updateMessage("It's a Tie!!")
+    elif(player == "rock"):
+        if(computer == "paper+button"):
+            updateMessage("you loose!!")
+            updateComputerscore()
+        else:
+            updateMessage("you win!!")
+            updateUserscore()
+    elif(player == "paper"):
+        if(computer == "rock_button"):
+            updateMessage("you  win!!")
+            updateUserscore()
+        else:
+            updateMessage("you loose!")
+            updateComputerscore()
+    elif(player == "scissor"):
+        if(computer == "rock"):
+            updateMessage("you loose!!")
+            updateComputerscore()
+        else:
+            updateMessage("You win !!")
+            updateUserscore()
+    else:
+        pass
+
+#choices
+choices = ["rock","paper","scissor"]
+def updateChoice(x):
+    #computer choices
+    computer_choice = choice(choices)
+    if(computer_choice =="rock"):
+        computer_image_label.config(image=Rock_img_computer)
+    elif(computer_choice =="paper"):
+        computer_image_label.config(image=paper_img_computer)
+    else:
+        computer_image_label.config(image=scissor_img_computer)
+
+    #user choice
+    if(x == "rock"):
+        user_image_label.config(image=Rock_img_user)
+    elif(x =="paper"):
+        user_image_label.configure(image=paper_img_user)
+    else:
+        user_image_label.configure(image=scissor_img_user)
+    checkWin(x,computer_choice)
+
+# button
+rock = Button(root,command=lambda:updateChoice("rock"),font="bold", width=20,height=2,text="ROCK",fg="black")
+rock.grid(row=2,column=1,padx=3)
+paper = Button(root,command=lambda:updateChoice("paper"),font="bold",width=20,height=2,text="PAPER",fg="black")
+paper.grid(row=2,column=2,padx=3)
+scissor = Button(root, command=lambda:updateChoice("scissor"),width=20,height=2,font="bold",text="SCISSOR",fg="black")
+scissor.grid(row=2,column=3,padx=3)
+
+root.mainloop()
